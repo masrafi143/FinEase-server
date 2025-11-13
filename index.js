165c515c695b86af254adf38cb761458aae66307
masrafi143 <masrafiofficial143@gmail.com>
2025-11-13 23:48:12 +0600

    finalize server code

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// FinEaseDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vrmmuai.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -31,7 +30,6 @@ async function run() {
 
     const db = client.db("finease_db");
     const transactionCollection = db.collection("transactions");
-    //const bidsCollection = db.collection("bids");
     const usersCollection = db.collection("users");
     // USER APIs
     app.post("/users", async (req, res) => {
@@ -75,24 +73,6 @@ async function run() {
       res.send(result);
     });
 
-    // Treansactions APIs
-    // app.get("/transactions", async (req, res) => {
-    //   // const projectField = {title: 1, price_min:1, price_max:1, image:1};
-    //   // const cursor = productsCollection.find().sort({price_min : 1}).skip(2).limit(2).project(projectField);
-    //   const cursor = transactionCollection.find();
-    //   const result = await cursor.toArray();
-    //   res.send(result);
-    // });
-
-    //     // latest products
-    //     app.get("/latest-products", async (req, res) => {
-    //       const cursor = productsCollection
-    //         .find()
-    //         .sort({ created_at: -1 })
-    //         .limit(6);
-    //       const result = await cursor.toArray();
-    //       res.send(result);
-    //     });
     app.get("/transactions/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -177,27 +157,6 @@ async function run() {
       const result = await transactionCollection.updateOne(query, update);
       res.send(result);
     });
-
-    //     app.delete("/products/:id", async (req, res) => {
-    //       const id = req.params.id;
-    //       const query = { _id: new ObjectId(id) };
-    //       const result = await productsCollection.deleteOne(query);
-    //       res.send(result);
-    //     });
-
-    //     app.get("/products/bids/:productId", async (req, res) => {
-    //       const productId = req.params.productId;
-    //       const query = { product: productId };
-    //       const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
-    //       const result = await cursor.toArray();
-    //       res.send(result);
-    //     });
-
-    //     app.post("/bids", async (req, res) => {
-    //       const myBid = req.body;
-    //       const result = await bidsCollection.insertOne(myBid);
-    //       res.send(result);
-    //     });
 
     // delete-transaction
     app.delete("/transactions/:id", async (req, res) => {
